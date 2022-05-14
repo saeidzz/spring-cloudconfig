a55cd59344a1850638461349d6f9780d7e8f8de1
saeid <szxhigh@gmail.com>
2022-05-14 13:57:50 +0430

    first commit

---
+++ src/test/java/com/sample/sampleconfigserver/SampleConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.sampleconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
